377dc9557f270ce0e86965dedbbf942937d5bf4d
abrarmir95 <mirabrar1995@gmail.com>
2023-03-15 14:13:22 +0530

    texteditor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TextEditor from "./components/TextEditor";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar />
+      <div className="editor">
+    <h2 className="h2">Text Editor</h2>
+        <TextEditor />
+      </div>
     </div>
   );
 }
 
 export default App;
+
